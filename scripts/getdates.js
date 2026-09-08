da0779961dc5b9cbcc95bf58385339911d5a2251
Debela Mendara <d.mendara5@gmail.com>
2026-09-08 21:50:34 +0300

    Add files via upload

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,7 @@
+// Output current year dynamically
+const currentYearSpan = document.getElementById("currentyear");
+currentYearSpan.textContent = new Date().getFullYear();
 
+// Output document last modified date dynamically
+const lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
